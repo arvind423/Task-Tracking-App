5ffc2da92b58b3795a9d6af9131050af7576d1c6
arvind427 <arvindmenariya89@gmail.com>
2022-12-05 16:59:21 +0530

    db.json file updated and mock json server has been removed

--- src/App.js
+++ src/App.js
@@ -8,84 +8,55 @@ import About from "./components/About";
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false);
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "doctor appointments",
+      day: "5th feb",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "doctor appointments",
+      day: "5th feb",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "doctor appointments",
+      day: "5th feb",
+      reminder: true,
+    },
+  ]);
 
-  useEffect(() => {
-    const getTasks = async () => {
-      const tasksFromServer = await fetchTasks();
-      setTasks(tasksFromServer);
-    };
-
-    getTasks();
-  }, []);
-
-  // Fetch Tasks
-  const fetchTasks = async () => {
-    const res = await fetch("http://localhost:5000/tasks");
-    const data = await res.json();
-
-    return data;
-  };
-
-  // Fetch Task
-  const fetchTask = async (id) => {
-    const res = await fetch(`http://localhost:5000/tasks/${id}`);
-    const data = await res.json();
-
-    return data;
-  };
+  
+  
+  
 
   // Add Task
-  const addTask = async (task) => {
-    const res = await fetch("http://localhost:5000/tasks", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(task),
-    });
-
-    const data = await res.json();
-
-    setTasks([...tasks, data]);
+    const addTask=()=>{
+      const id = Math.floor(Math.random() * 10000) + 1;
+      const newTask={id,...tasks};
+      setTasks([...tasks,newTask]);
+    }
 
     // const id = Math.floor(Math.random() * 10000) + 1
     // const newTask = { id, ...task }
     // setTasks([...tasks, newTask])
-  };
+  
 
   // Delete Task
-  const deleteTask = async (id) => {
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: "DELETE",
-    });
-    //We should control the response status to decide if we will change the state or not.
-    res.status === 200
-      ? setTasks(tasks.filter((task) => task.id !== id))
-      : alert("Error Deleting This Task");
-  };
+
+    const deleteTask=(id)=>{
+      setTasks(tasks.filter((task)=>task.id!==id));
+    } 
 
   // Toggle Reminder
-  const toggleReminder = async (id) => {
-    const taskToToggle = await fetchTask(id);
-    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+  
+    const toggleReminder=()=>{
 
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: "PUT",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(updTask),
-    });
-
-    const data = await res.json();
-
-    setTasks(
-      tasks.map((task) =>
-        task.id === id ? { ...task, reminder: data.reminder } : task
-      )
-    );
-  };
+    }
+   
 
   return (
     <Router>
