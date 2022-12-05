17c888b80d425034fd4d38106fcae9238c370bf2
arvind427 <arvindmenariya89@gmail.com>
2022-12-05 17:56:05 +0530

    useeffect hook removed from app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
